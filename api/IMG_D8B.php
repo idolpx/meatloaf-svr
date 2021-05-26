a273a0e8ebdd91526d0980af12d2b757530f9a7e
James Johnston <jjohnston@techknowpro.com>
2021-05-26 01:11:41 -0500

    Update attribution

--- api/IMG_D8B.php
+++ api/IMG_D8B.php
@@ -1,4 +1,21 @@
 <?php
+// Meatloaf Server
+// https://github.com/idolpx/meatloaf-svr
+// Copyright(C) 2020 James Johnston
+//
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
 require_once("IMG_native.php");
 
 class IMG_D8B extends IMG_D64 {
