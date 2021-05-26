2bf49b0f240fd720057325de73e3ffbd0bc8f9cd
idolpx <jaime@idolpx.com>
2021-05-26 00:48:54 -0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,37 +1,26 @@
 <?php
-/*
- *  Meat Loaf Server
- *  ---------------
- *
- *  Copyright (C) 2020, Jaime Johnston <jaime@idolpx.com>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
 
+// PRG File
 require_once("IMG_NATIVE.php");
 require_once("IMG_P00.php");
+
+// Disk Image
 require_once("IMG_D64.php");
 require_once("IMG_D71.php");
 require_once("IMG_D81.php");
 require_once("IMG_D8B.php");
+
+// Tape Image
 require_once("IMG_T64.php");
 require_once("IMG_TCRT.php");
 
-$url = "C64.IDOLPX.COM";
-$root = $_SERVER['DOCUMENT_ROOT']."/64/";
+// Cartridge Image
+require_once("IMG_CRT.php");
+
+
+
+$url = "C64.MEATLOAF.CC";
+$root = $_SERVER['DOCUMENT_ROOT']."/roms/";
 
 
 $path = $_GET['p'].$_POST['p'];
@@ -43,8 +32,7 @@ $filename = $_GET['f'].$_POST['f'];
 if ($filename == '')
 	$filename = '$';
 
-
-$disk_name = "MEATLOAF 64 ARCHIVE"; // Set default header
-$show_nfo = true;		    // Show selected URL, PATH, IMAGE in directory listing
-$show_hidden = false;	            // Show hidden/deleted files in directory listing
+$disk_name = "MEATLOAF C64 ARCHIVE";
+$show_nfo = true;		// Show selected URL, PATH, IMAGE in directory listing
+$show_hidden = false;	// Show hidden/deleted files in directory listing
 ?>
